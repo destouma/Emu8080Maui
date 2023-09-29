f4215dc30fc2ece84a5429bc1c999ed712dcc58c
destouma <mdsoftengr@gmail.com>
2023-09-29 11:39:29 -0400

    first commit

---
+++ Emu8080Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Emu8080Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Emu8080Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Emu8080Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
